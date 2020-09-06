8e492cbe8ed6f4143af9406fa6135416f19afcde
shota <s01-work51@outlook.jp>
2020-09-06 14:37:46 +0000

    model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_103438) do
+ActiveRecord::Schema.define(version: 2020_09_06_143150) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
